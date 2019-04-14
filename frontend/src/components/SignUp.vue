c33cf9ceaa04add6f5e699929b4846f4036284bd
kiana <kkia1996@gmail.com>
2019-04-14 15:50:49 +0200

    ***

---
+++ frontend/src/components/SignUp.vue
@@ -0,0 +1,55 @@
+<template lang="html">
+  <div class="inscription">
+      <form class="w-50 mx-auto">
+        <h1>Créer un compte</h1>
+          <div class="form-group">
+              <label>Prénom</label>
+              <input class="form-control" type="text" v-model="prenom">
+          </div>
+          <div class="form-group">
+              <label>Nom</label>
+              <input class="form-control" type="text" v-model="nom">
+          </div>
+          <div class="form-group">
+              <label>Email</label>
+              <input class="form-control" type="number" v-model="email">
+          </div>
+          <div class="form-group">
+              <label>Mot de passe</label>
+              <input class="form-control" type="number" v-model="mdp">
+          </div>
+          <div class="form-group">
+              <label>Confirmation de mot de passe</label>
+              <input class="form-control" type="number" v-model="confirmation">
+          </div>
+          <button class="btn btn-success" type="button" name="button" v-on:click="SignUp(prenom, nom, email, mdp ,confirmation)">Créer</button>
+      </form>
+  </div>
+</template>
+
+<script>
+
+import *********************** from '../services/************************.vue'
+
+export default {
+  data () {
+    return {
+    prenom: '' ,
+    nom: '' ,
+    email: '',
+    mdp: '',
+    confirmation: ''
+    }
+  },
+  methods: {
+    ************: function (prenom, nom, email, mdp, confirmation) {
+      ************.SignUp(prenom, nom, email, mdp, confirmation).then(alert("created!"));
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+
+
+</style>
